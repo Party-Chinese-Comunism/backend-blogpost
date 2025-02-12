368dc174334fb71ac65219ab34620d606374cfbd
roberto <rvpassos@hotmail.com>
2025-02-12 20:41:47 -0300

    favorite post function

--- models/models.py
+++ models/models.py
@@ -1,14 +1,21 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+favorites = db.Table('favorites', 
+    db.Column('user.id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('post.id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
     
-    posts = db.relationship('Post', backref='author', lazy=True)  # ✅ Apenas esta referência é necessária
+    posts = db.relationship('Post', backref='author', lazy=True)
     comments = db.relationship('Comment', backref='commenter', lazy=True)
+
+    favorites = db.relationship('Post', secondary=favorites, backref='favorited_by', lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
