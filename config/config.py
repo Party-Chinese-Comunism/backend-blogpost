dd13b04bb2ed9719037a67035003884204fbf9d0
Andre G Chang <104398034+AndreGChang@users.noreply.github.com>
2025-02-13 15:57:28 -0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -10,5 +10,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///site.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secreto")
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=5) # Define o tempo de vida do token de acesso (30 minutos)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=60) # Define o tempo de vida do token de acesso (30 minutos)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=7) # Define o tempo de vida do token de refresh (7 dias)
