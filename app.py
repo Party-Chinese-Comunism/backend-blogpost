bc621ca429cc9adb524ef4d672a26b38b976f5e4
Andre G Chang <104398034+AndreGChang@users.noreply.github.com>
2025-05-11 18:44:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,12 @@ def create_app(testing=False):
 
     db.init_app(app)
     jwt.init_app(app)
-    CORS(app, resources={r"/api/*": {"origins": "https://frontend.35.231.215.237.nip.io"}}, supports_credentials=True)
+    CORS(app,
+     resources={r"/api/*": {"origins": "https://frontend.35.231.215.237.nip.io"}},
+     supports_credentials=True,
+     allow_headers=["Content-Type", "Authorization"],
+     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+
     migrate.init_app(app, db)
 
     IMAGE_FOLDER = os.path.join(os.getcwd(), "uploads")
