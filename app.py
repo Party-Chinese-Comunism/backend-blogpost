cf5216e720b611d531dd1b01e2f86c63e412e3da
AndreGChang <FireWitch2003@outlook.com>
2025-05-16 23:07:14 -0300

    alterando tamanho de envio de arquivo

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ migrate = Migrate()
 
 def create_app(testing=False):
     app = Flask(__name__)
+    app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # Limite de 20MB
+
     if testing:
         app.config.from_object(TestingConfig)
     else:
